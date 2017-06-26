033f390783c4f5c3cf8dbaf21ed301b7aaf83222
ashutosh <optimustechproject201709@gmail.com>
2017-06-26 18:11:51 +0530

    course selection added

---
+++ app/src/main/java/com/software_solutions/optimus_tech_project201709/Course.java
@@ -0,0 +1,24 @@
+package com.software_solutions.optimus_tech_project201709;
+
+/**
+ * Created by H.P on 25-Jun-17.
+ */
+
+public class Course {
+
+    private String courseName;
+
+    public Course(String courseName) {
+        this.courseName = courseName;
+
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+}
---
+++ app/src/main/java/com/software_solutions/optimus_tech_project201709/ItemClickListener.java
@@ -0,0 +1,12 @@
+package com.software_solutions.optimus_tech_project201709;
+
+import android.view.View;
+
+/**
+ * Created by H.P on 26-Jun-17.
+ */
+
+public interface ItemClickListener {
+
+    void onItemClick(View v, int pos);
+}
